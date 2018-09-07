64f8917b31249bf54e961c36734288707e517f55
System Administrator <root@benjamins99-mbp.student.iastate.edu>
2018-09-07 12:07:10 -0500

    daily updates

--- lab2b.c
+++ lab2b.c
@@ -4,19 +4,20 @@
 int main()
 {
         //declare section
-	float onenum;
+	float onenum = 0.0;
 	float *A;
-	double twonum;
-	double *B;	
-        //print and scan section
+	double twonum = 0.0;
+	double *B;
+	//print and scan section
         printf("Enter Number to convert to scientific notation: \n");
-        scanf("%lf", &twonum );
+        scanf("%f", &onenum );
+	A = &onenum;
+        B = &twonum;
 	twonum = onenum;
-	*A = onenum;
         printf("Here is the normal float:\n");
-	printf("%f*/n", &twonum);
+	printf("%f\n", *A);
 	printf("Here is the scientific notation:\n");
-	printf("%E*\n", &onenum);
-	printf("%e*\n", &onenum);
+	printf("%E\n", *B );
+	printf("%e\n", *B );
 }
 
